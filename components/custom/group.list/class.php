5a3705102dcfb206c208352e61efe97894f4f23e
DOA0110 <88523919+DOA0110@users.noreply.github.com>
2021-08-11 13:35:14 +0700

    complex components group users

---
+++ components/custom/group.list/class.php
@@ -0,0 +1,71 @@
+<?
+use \Bitrix\Main;
+use \Bitrix\Main\Loader;
+use \Bitrix\Main\Error;
+use \Bitrix\Main\Type\DateTime;
+use \Bitrix\Main\Localization\Loc;
+use \Bitrix\Iblock\Component\Base;
+use \Bitrix\Iblock\Component\ElementList;
+
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @global CUser $USER
+ * @global CMain $APPLICATION
+ * @global CIntranetToolbar $INTRANET_TOOLBAR
+ */
+
+Loc::loadMessages(__FILE__);
+
+if (!\Bitrix\Main\Loader::includeModule('iblock'))
+{
+	ShowError(Loc::getMessage('IBLOCK_MODULE_NOT_INSTALLED'));
+	return;
+}
+
+
+class XUserList extends ElementList
+{
+    public function executeComponent()
+    {
+       
+        $this->checkModules();
+        
+        if ($this->hasErrors())
+        {
+            return $this->processErrors();
+        }
+        
+        if ($this->isCacheDisabled() || $this->startResultCache(false, $this->getAdditionalCacheId(), $this->getComponentCachePath()))
+        {
+            
+            $result = \Bitrix\Main\GroupTable::getList(array(
+                'filter' => array('ACTIVE'=>'Y'),
+                'select' => array('*'),
+                'order' => array('ID'=>'ASC'), 
+                'cache' => array(
+                    'ttl' => 60
+                )
+            ));
+            while ($arGroup = $result->fetch()) 
+            {
+                $this->arResult['ITEMS'][] = $arGroup;
+            }
+            
+            if (!$this->hasErrors())
+            {
+                
+                $this->initResultCache();
+                $this->includeComponentTemplate();
+                
+            }
+        }
+        if ($this->hasErrors())
+        {
+            return $this->processErrors();
+        }
+        return !empty($this->arResult['ITEMS']) ? true : false;
+    }
+}
+?>
